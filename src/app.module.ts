f519bf0572a688a37033af55123ca83698ea0a06
arjun <gaurarjun444@gmail.com>
2023-12-24 22:12:57 +0530

    fix root url

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ApiModule } from './api/api.module';
 import { ClickhouseService } from './api/clickhouse/clickhouse.service';
+import { AppController } from './app.controller';
 @Module({
   imports: [ApiModule, ConfigModule.forRoot()],
   providers: [ClickhouseService],
+  controllers: [AppController],
 })
 export class AppModule {}
